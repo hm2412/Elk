4b7d7daeadae2ccb381aef6a52774f5670365d30
Haleema Mohammed <mohammedhaleema24@gmail.com>
2024-12-05 19:08:49 +0000

    Order requests chronologically

--- tutorials/helpers.py
+++ tutorials/helpers.py
@@ -29,7 +29,7 @@ def admin_dashboard_context():
     total_students = User.objects.filter(user_type='Student').count()
     total_tutors = User.objects.filter(user_type='Tutor').count()
     '''requests = Request.objects.filter(student__user_type='Student').order_by('-submitted_at')'''
-    requests = Lesson.objects.all()
+    requests = Lesson.objects.all().order_by('-created_at')
     return {
         'total_students': total_students,
         'total_tutors': total_tutors,
