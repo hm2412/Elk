11ca7967f5809c30ee3a925027f65392653f48df
Haleema Mohammed <mohammedhaleema24@gmail.com>
2024-12-05 16:33:12 +0000

    Link admin to lesson requests

--- tutorials/helpers.py
+++ tutorials/helpers.py
@@ -1,8 +1,7 @@
 from django.conf import settings
 from django.shortcuts import redirect
 from django.core.exceptions import PermissionDenied
-from .models import User
-from .models import Request
+from .models import User, Request, Lesson
 
 def login_prohibited(view_function):
     """Decorator for view functions that redirect users away if they are logged in."""
@@ -29,7 +28,8 @@ def user_role_required(type):
 def admin_dashboard_context():
     total_students = User.objects.filter(user_type='Student').count()
     total_tutors = User.objects.filter(user_type='Tutor').count()
-    requests = Request.objects.filter(student__user_type='Student').order_by('-submitted_at')
+    '''requests = Request.objects.filter(student__user_type='Student').order_by('-submitted_at')'''
+    requests = Lesson.objects.all()
     return {
         'total_students': total_students,
         'total_tutors': total_tutors,
