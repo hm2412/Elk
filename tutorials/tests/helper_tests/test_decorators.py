d7a8aea179362e567011dd3d0beabe0b1262d7ec
Haleema Mohammed <mohammedhaleema24@gmail.com>
2024-12-11 12:35:39 +0000

    Fix test case

--- tutorials/tests/helper_tests/test_decorators.py
+++ tutorials/tests/helper_tests/test_decorators.py
@@ -5,21 +5,15 @@ from tutorials.models import User
 from tutorials.helpers import login_prohibited
 
 class LoginProhibitedTests(TestCase):
+    fixtures = ['tutorials/tests/fixtures/default_user.json']
+
     def setUp(self):
         self.home_url = reverse('home')
-        self.user = {
-                'first_name': 'Test',
-                'last_name': 'User',
-                'username': '@testuser',
-                'email': 'testuser@example.org',
-                'new_password': 'Password123',
-                'password_confirmation': 'Password123',
-                'user_type': 'Student'
-        }
+        self.user = User.objects.get(username='@johndoe')
         self.redirect_url = reverse(settings.REDIRECT_URL_WHEN_LOGGED_IN)
 
     def test_redirect_logged_in_user(self):
-        self.client.login(username='@testuser', password='Password123')
+        self.client.login(username=self.user.username, password='Password123')
 
         response = self.client.get(self.home_url)
         self.assertRedirects(response, self.redirect_url, status_code=302, target_status_code=200)
