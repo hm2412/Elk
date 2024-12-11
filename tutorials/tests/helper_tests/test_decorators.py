7cd51a78971fb90b9a58bbfdf7978d3caa03c5f3
Haleema Mohammed <mohammedhaleema24@gmail.com>
2024-12-11 12:55:09 +0000

    Add UserRoleRequired test

--- tutorials/tests/helper_tests/test_decorators.py
+++ tutorials/tests/helper_tests/test_decorators.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
+from django.core.exceptions import PermissionDenied
 from tutorials.models import User
-from tutorials.helpers import login_prohibited
 
 class LoginProhibitedTests(TestCase):
     fixtures = ['tutorials/tests/fixtures/default_user.json']
@@ -17,3 +17,22 @@ class LoginProhibitedTests(TestCase):
 
         response = self.client.get(self.home_url)
         self.assertRedirects(response, self.redirect_url, status_code=302, target_status_code=200)
+
+class UserRoleRequiredTests(TestCase):
+    fixtures = ['tutorials/tests/fixtures/other_users.json']
+
+    def setUp(self):
+        self.student = User.objects.get(username='@charlie')
+        self.admin = User.objects.get(username='@petrapickles')
+        self.student_id = self.student.id
+        self.schedule_session_url = reverse('schedule_session', args=[self.student_id])
+
+    def test_student_access_denied(self):
+        self.client.login(username=self.student.username, password='Password123')
+        response = self.client.get(self.schedule_session_url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_tutor_access_granted(self):
+        self.client.login(username=self.admin.username, password='Password123')
+        response = self.client.get(self.schedule_session_url)
+        self.assertEqual(response.status_code, 200)
