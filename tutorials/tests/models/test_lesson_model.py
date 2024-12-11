6761379ae390ff0f6249ac1ea634ae4c1f050806
Rashida Begum (k23057024) <rashida_wb@outlook.com>
2024-12-11 14:55:56 +0000

    Update test_lesson_model

--- tutorials/tests/models/test_lesson_model.py
+++ tutorials/tests/models/test_lesson_model.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from datetime import datetime, time, timedelta  
 from tutorials.models import Lesson
 from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError 
 
 class LessonModelTestCase(TestCase):
 
@@ -15,23 +16,47 @@ class LessonModelTestCase(TestCase):
         )
 
     def test_lesson_save_sets_end_time_and_time_of_day(self):
-       
         lesson = Lesson(
             student=self.user,
             knowledge_area='Python',
             term='Sept-Dec',
-            start_time=time(9, 30),  
-            duration=90,  
+            start_time=time(9, 30),
+            duration=90,
             days=["mon", "wed"],
             venue_preference='online'
         )
         lesson.save()
-
-        expected_end_time = (timezone.make_aware(datetime.combine(datetime.today(), lesson.start_time)) + timedelta(minutes=lesson.duration)).time()
+        expected_end_time = (
+            timezone.make_aware(datetime.combine(datetime.today(), lesson.start_time))
+            + timedelta(minutes=lesson.duration)
+        ).time()
         self.assertEqual(lesson.end_time, expected_end_time)
-
         self.assertEqual(lesson.time_of_day, 'morning')
 
+    def test_lesson_save_without_start_time(self):
+        lesson = Lesson(
+            student=self.user,
+            knowledge_area='Python',
+            term='Sept-Dec',
+            start_time=None,  
+            duration=90,
+            days=["mon", "wed"],
+            venue_preference='online'
+        )
+        lesson.save()
+        self.assertIsNone(lesson.end_time)  
+
+    def test_lesson_save_without_duration(self):
+        with self.assertRaises(IntegrityError):
+            Lesson.objects.create(
+                student=self.user,
+                knowledge_area='Python',
+                term='Sept-Dec',
+                start_time=time(9, 30),
+                duration=None,  
+                days=["mon", "wed"],
+                venue_preference='online'
+            )
     def test_get_time_of_day_for_different_times(self):
     
         lesson_morning = Lesson(
@@ -66,6 +91,17 @@ class LessonModelTestCase(TestCase):
             venue_preference='onsite'
         )
         self.assertEqual(lesson_evening.get_time_of_day(), 'evening')
+
+        lesson_evening = Lesson(
+            student=self.user,
+            knowledge_area='Ruby',
+            term='Jan-April',
+            start_time=time(21, 30), 
+            duration=60,
+            days=["tue", "thu"],
+            venue_preference='onsite'
+        )
+        self.assertNotEqual(lesson_evening.get_time_of_day(), 'evening')
 
     def test_lesson_str_method(self):
         lesson = Lesson(
