7e34403c30e7d5f63cdbd997670f167faaf2a71f
Haleema Mohammed <mohammedhaleema24@gmail.com>
2024-12-11 12:08:56 +0000

    Reorganise views.py

--- tutorials/views.py
+++ tutorials/views.py
@@ -55,6 +55,7 @@ from .helpers import (
     render_user_list
 )
 
+# Dashboard views
 @login_required
 def dashboard(request):
     """Display the current user's dashboard."""
@@ -86,6 +87,135 @@ def dashboard(request):
 @login_prohibited
 def home(request):
     return render(request, 'home.html')
+
+# Sign up/log in handling
+
+class LoginProhibitedMixin:
+    """Mixin that redirects when a user is logged in."""
+
+    redirect_when_logged_in_url = None
+
+    def dispatch(self, *args, **kwargs):
+        """Redirect when logged in, or dispatch as normal otherwise."""
+        if self.request.user.is_authenticated:
+            return self.handle_already_logged_in(*args, **kwargs)
+        return super().dispatch(*args, **kwargs)
+
+    def handle_already_logged_in(self, *args, **kwargs):
+        url = self.get_redirect_when_logged_in_url()
+        return redirect(url)
+
+    def get_redirect_when_logged_in_url(self):
+        """Returns the url to redirect to when not logged in."""
+        if self.redirect_when_logged_in_url is None:
+            raise ImproperlyConfigured(
+                "LoginProhibitedMixin requires either a value for "
+                "'redirect_when_logged_in_url', or an implementation for "
+                "'get_redirect_when_logged_in_url()'."
+            )
+        else:
+            return self.redirect_when_logged_in_url
+
+
+class LogInView(LoginProhibitedMixin, View):
+    """Display login screen and handle user login."""
+
+    http_method_names = ['get', 'post']
+    redirect_when_logged_in_url = settings.REDIRECT_URL_WHEN_LOGGED_IN
+
+    def get(self, request):
+        """Display log in template."""
+
+        self.next = request.GET.get('next') or ''
+        return self.render()
+
+    def post(self, request):
+        """Handle log in attempt."""
+
+        form = LogInForm(request.POST)
+        self.next = request.POST.get('next') or settings.REDIRECT_URL_WHEN_LOGGED_IN
+        user = form.get_user()
+        if user is not None:
+            login(request, user)
+            return redirect(self.next)
+        messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
+        return self.render()
+
+    def render(self):
+        """Render log in template with blank log in form."""
+
+        form = LogInForm()
+        return render(self.request, 'log_in.html', {'form': form, 'next': self.next})
+
+
+def log_out(request):
+    """Log out the current user"""
+
+    logout(request)
+    return redirect('home')
+
+
+class PasswordView(LoginRequiredMixin, FormView):
+    """Display password change screen and handle password change requests."""
+
+    template_name = 'password.html'
+    form_class = PasswordForm
+
+    def get_form_kwargs(self, **kwargs):
+        """Pass the current user to the password change form."""
+
+        kwargs = super().get_form_kwargs(**kwargs)
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+    def form_valid(self, form):
+        """Handle valid form by saving the new password."""
+
+        form.save()
+        login(self.request, self.request.user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        """Redirect the user after successful password change."""
+
+        messages.add_message(self.request, messages.SUCCESS, "Password updated!")
+        return reverse('dashboard')
+    
+class SignUpView(LoginProhibitedMixin, FormView):
+    """Display the sign up screen and handle sign ups."""
+
+    form_class = SignUpForm
+    template_name = "sign_up.html"
+    redirect_when_logged_in_url = settings.REDIRECT_URL_WHEN_LOGGED_IN
+
+    def form_valid(self, form):
+        self.object = form.save()
+        login(self.request, self.object)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse(settings.REDIRECT_URL_WHEN_LOGGED_IN)
+
+# Profile handling
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    """Display user profile editing screen, and handle profile modifications."""
+
+    model = UserForm
+    template_name = "profile.html"
+    form_class = UserForm
+
+    def get_object(self):
+        """Return the object (user) to be updated."""
+        user = self.request.user
+        return user
+
+    def get_success_url(self):
+        """Return redirect URL after successful update."""
+        messages.add_message(self.request, messages.SUCCESS, "Profile updated!")
+        return reverse(settings.REDIRECT_URL_WHEN_LOGGED_IN)
+
+# Tutor dashboard view functions
 
 @login_required
 def tutor_availability(request):
@@ -213,134 +343,8 @@ def set_availability(request):
         return JsonResponse({'success': True})
     except Exception as e:
         return JsonResponse({'success': False, 'error': str(e)}, status=400)
-
-
-class LoginProhibitedMixin:
-    """Mixin that redirects when a user is logged in."""
-
-    redirect_when_logged_in_url = None
-
-    def dispatch(self, *args, **kwargs):
-        """Redirect when logged in, or dispatch as normal otherwise."""
-        if self.request.user.is_authenticated:
-            return self.handle_already_logged_in(*args, **kwargs)
-        return super().dispatch(*args, **kwargs)
-
-    def handle_already_logged_in(self, *args, **kwargs):
-        url = self.get_redirect_when_logged_in_url()
-        return redirect(url)
-
-    def get_redirect_when_logged_in_url(self):
-        """Returns the url to redirect to when not logged in."""
-        if self.redirect_when_logged_in_url is None:
-            raise ImproperlyConfigured(
-                "LoginProhibitedMixin requires either a value for "
-                "'redirect_when_logged_in_url', or an implementation for "
-                "'get_redirect_when_logged_in_url()'."
-            )
-        else:
-            return self.redirect_when_logged_in_url
-
-
-class LogInView(LoginProhibitedMixin, View):
-    """Display login screen and handle user login."""
-
-    http_method_names = ['get', 'post']
-    redirect_when_logged_in_url = settings.REDIRECT_URL_WHEN_LOGGED_IN
-
-    def get(self, request):
-        """Display log in template."""
-
-        self.next = request.GET.get('next') or ''
-        return self.render()
-
-    def post(self, request):
-        """Handle log in attempt."""
-
-        form = LogInForm(request.POST)
-        self.next = request.POST.get('next') or settings.REDIRECT_URL_WHEN_LOGGED_IN
-        user = form.get_user()
-        if user is not None:
-            login(request, user)
-            return redirect(self.next)
-        messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
-        return self.render()
-
-    def render(self):
-        """Render log in template with blank log in form."""
-
-        form = LogInForm()
-        return render(self.request, 'log_in.html', {'form': form, 'next': self.next})
-
-
-def log_out(request):
-    """Log out the current user"""
-
-    logout(request)
-    return redirect('home')
-
-
-class PasswordView(LoginRequiredMixin, FormView):
-    """Display password change screen and handle password change requests."""
-
-    template_name = 'password.html'
-    form_class = PasswordForm
-
-    def get_form_kwargs(self, **kwargs):
-        """Pass the current user to the password change form."""
-
-        kwargs = super().get_form_kwargs(**kwargs)
-        kwargs.update({'user': self.request.user})
-        return kwargs
-
-    def form_valid(self, form):
-        """Handle valid form by saving the new password."""
-
-        form.save()
-        login(self.request, self.request.user)
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        """Redirect the user after successful password change."""
-
-        messages.add_message(self.request, messages.SUCCESS, "Password updated!")
-        return reverse('dashboard')
-
-
-class ProfileUpdateView(LoginRequiredMixin, UpdateView):
-    """Display user profile editing screen, and handle profile modifications."""
-
-    model = UserForm
-    template_name = "profile.html"
-    form_class = UserForm
-
-    def get_object(self):
-        """Return the object (user) to be updated."""
-        user = self.request.user
-        return user
-
-    def get_success_url(self):
-        """Return redirect URL after successful update."""
-        messages.add_message(self.request, messages.SUCCESS, "Profile updated!")
-        return reverse(settings.REDIRECT_URL_WHEN_LOGGED_IN)
-
-
-class SignUpView(LoginProhibitedMixin, FormView):
-    """Display the sign up screen and handle sign ups."""
-
-    form_class = SignUpForm
-    template_name = "sign_up.html"
-    redirect_when_logged_in_url = settings.REDIRECT_URL_WHEN_LOGGED_IN
-
-    def form_valid(self, form):
-        self.object = form.save()
-        login(self.request, self.object)
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse(settings.REDIRECT_URL_WHEN_LOGGED_IN)
     
-""" Views for Student Dashboard below"""
+# Student dashboard view functions
 
 def create_lesson_request(request):
     if request.method == 'POST':
@@ -442,6 +446,8 @@ class AddCustomSubjectView(LoginRequiredMixin, FormView):
         messages.success(self.request, f'Added custom subject: {custom_subject}')
         return super().form_valid(form)
 
+# User list handling
+
 def user_list(request, list_type):
     if request.user.user_type == 'Student':
         return HttpResponseForbidden("You do not have permission to access this page.")
@@ -458,7 +464,8 @@ def user_list(request, list_type):
     users = paginate_users(request, users)
     return render_user_list(request, users, title, filters)
 
-"""Admin dashboard view functions"""
+# Admin dashboard view functions
+
 @login_required
 @user_role_required('Admin')
 def schedule_session(request, student_id):
