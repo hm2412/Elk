e3e600aee5bfa6749c622afe3093a1d191fd5189
Rashida Begum (k23057024) <rashida_wb@outlook.com>
2024-12-12 21:00:32 +0000

    Update test_tutor_subjects_view

--- tutorials/tests/views/test_tutor_subjects_view.py
+++ tutorials/tests/views/test_tutor_subjects_view.py
@@ -1,10 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.contrib.messages import get_messages
 from tutorials.models import TutorProfile
+from django.contrib.messages.storage.fallback import FallbackStorage
+from tutorials.views import TutorSubjectsView
+from django.contrib.messages.middleware import MessageMiddleware
+from django.contrib.sessions.middleware import SessionMiddleware
 
-class TutorSubjectsViewTests(TestCase):
+class TutorSubjectsFunctionViewTests(TestCase):
     fixtures = ['tutorials/tests/fixtures/other_users.json']
 
     def setUp(self):
@@ -42,3 +46,54 @@ class TutorSubjectsViewTests(TestCase):
     def test_tutor_subjects_not_logged_in(self):
         response = self.client.post(self.url, {'subjects': ['Ruby']})
         self.assertRedirects(response, reverse('log_in') + f'?next={self.url}')
+
+
+class TutorSubjectsClassViewTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = get_user_model().objects.create_user(username='@janedoe', password='Password123')
+        self.profile, _ = TutorProfile.objects.get_or_create(tutor=self.user, subjects=['Java', 'Ruby'])
+
+    def test_get_initial(self):
+        request = self.factory.get(reverse('tutor_subjects'))
+        request.user = self.user
+        view = TutorSubjectsView()
+        view.setup(request)
+        initial = view.get_initial()
+
+        profile = TutorProfile.objects.get(tutor=self.user)
+        self.assertEqual(initial['subjects'], profile.subjects)
+
+
+    def test_form_valid_updates_profile(self):
+        request = self.factory.post(reverse('tutor_subjects'), {
+            'subjects': ['Java', 'Ruby', 'Python/Tensorflow']
+        })
+        request.user = self.user
+
+        session_middleware = SessionMiddleware(get_response=lambda req: None)
+        session_middleware.process_request(request)
+        request.session.save()
+
+        messages_middleware = MessageMiddleware(get_response=lambda req: None)
+        messages_middleware.process_request(request)
+        setattr(request, '_messages', FallbackStorage(request))
+
+        view = TutorSubjectsView()
+        view.setup(request)
+
+        form = view.get_form_class()(data=request.POST)
+        self.assertTrue(form.is_valid())
+
+        response = view.form_valid(form)
+
+        profile = TutorProfile.objects.get(tutor=self.user)
+
+        self.assertEqual(profile.subjects, ['Java', 'Ruby', 'Python/Tensorflow'])
+        self.assertEqual(response.status_code, 302) 
+        self.assertEqual(response.url, reverse('dashboard'))
+
+
+        messages = list(request._messages)
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].message, 'Teaching subjects updated successfully.')
